29938d290bc791c502b7c064fd675bdab58f3b61
Rodolphe Marbot <rododhendron@hey.com>
2024-12-02 08:30:26 +0100

    gabaa

--- scripts/main.jl
+++ scripts/main.jl
@@ -8,19 +8,22 @@ using Symbolics, ModelingToolkit, DifferentialEquations
 using .Neuron
 
 tspan = (0, 1)
-n_neurons = 1000
+n_neurons = 10
 i_neurons = 2
 e_neurons = 8
 
-@time params = Neuron.AdExNeuronParams(;input_value=1e-9)
-@time neurons = [neuron = Neuron.make_neuron(params, Neuron.Soma, tspan, Symbol("neuron_$(i)")) for i in 1:n_neurons]
+@time params = Neuron.AdExNeuronParams(; input_value=1e-9)
+@time i_neurons = [Neuron.make_neuron(params, Neuron.Soma, tspan, Symbol("i_neuron_$(i)")) for i in 1:i_neurons]
+@time e_neurons = [Neuron.make_neuron(params, Neuron.Soma, tspan, Symbol("e_neuron_$(i)")) for i in 1:e_neurons]
 
-@time network = Neuron.make_network(neurons)
+ee_connections = Neuron.connect_many_neurons(e_neurons, e_neurons, Neuron.AMPA(), 0.5)
+ei_connections = Neuron.connect_many_neurons(e_neurons, i_neurons, Neuron.AMPA(), 0.5)
+ie_connections = Neuron.connect_many_neurons(e_neurons, i_neurons, Neuron.GABAa(), 1.0)
+
+@time network = Neuron.make_network(vcat(e_neurons, i_neurons), vcat(ee_connections, ei_connections, ie_connections))
 
 simplified_model = network
 # @time simplified_model = Neuron.structural_simplify(network)
-println("jac")
-@time calculate_jacobian(simplified_model)
 
 # infere params
 @time uparams = Neuron.AdExNeuronUParams()
@@ -29,13 +32,6 @@ println("jac")
 
 # a = [iparams[1:end-1]; simplified_model.neuron_3.soma.input_value => 1e-9]
 # resolve
-@time prob = ODEProblem(simplified_model, iuparams, tspan, iparams, jac=true, sparse=true)
+@time prob = ODEProblem(simplified_model, iuparams, tspan, iparams)
 
-function n_times_solve(problem, n)
-    for i in 1:n
-        solve(prob, Vern6(); abstol=1e-7, reltol=1e-7)
-    end
-end
-
-sol = solve(prob, Vern6(); abstol=1e-7, reltol=1e-7)
-@time time_to_run = n_times_solve(prob, 10)
+sol = solve(prob, Vern6(); abstol=1e-4, reltol=1e-4)
--- src/neuron.jl
+++ src/neuron.jl
@@ -1,5 +1,5 @@
 module Neuron
-using ModelingToolkit, Parameters
+using ModelingToolkit, Parameters, StringDistances
 using ModelingToolkit: t_nounits as t, D_nounits as D
 
 const start_input::Float64 = 200e-3
@@ -12,7 +12,7 @@ step_fn(t, iv) = start_input < t < start_input + input_duration ? iv : 0
         v(t) = -65e-3
         w(t) = 0
         Ie(t), [input = true]
-        # Ii(t), [input = true]
+        Ii(t), [input = true]
         Ib(t), [input = true]
         R(t), [output = true]
     end
@@ -29,7 +29,7 @@ step_fn(t, iv) = start_input < t < start_input + input_duration ? iv : 0
         input_value
     end
     @equations begin
-        D(v) ~ (-Je * (v - vrest) + Je * delta * exp((v - vthr) / delta) - w + Ie + Ib) / Cm # voltage dynamic
+        D(v) ~ (-Je * (v - vrest) + Je * delta * exp((v - vthr) / delta) - w + Ii + Ie + Ib) / Cm # voltage dynamic
         D(w) ~ (-w + a * (v - vrest)) / TauW # adaptation dynamic
         Ib ~ step_fn(t, input_value)
         D(R) ~ 0
@@ -39,7 +39,7 @@ end
 @mtkmodel SynapseAMPA begin # AMPA synapse https://neuronaldynamics.epfl.ch/online/Ch3.S1.html#Ch3.E2
     @parameters begin
         tau_ampa
-        vtarget
+        vtarget_exc
         inc_gsyn
     end
     @variables begin
@@ -51,17 +51,19 @@ end
     end
 end
 
-@mtkmodel SynapseGABAa begin # AMPA synapse https://neuronaldynamics.epfl.ch/online/Ch3.S1.html#Ch3.E2
+@mtkmodel SynapseGABAa begin # GABAa synapse https://neuronaldynamics.epfl.ch/online/Ch3.S1.html#Ch3.E2
     @parameters begin
-        gmax
-        tau
+        tau_GABAa_rise
+        tau_GABAa_fast
+        vtarget_inh
+        inc_gsyn
     end
     @variables begin
-        g_syn(t)
+        g_syn(t) = 0, [input = true]
     end
     # have to bind I on connect
     @equations begin
-        D(g_syn) ~ -g_syn / tau
+        D(g_syn) ~ -g_syn / tau_GABAa_rise + g_syn / tau_GABAa_fast
     end
 end
 
@@ -80,7 +82,10 @@ Base.@kwdef struct AdExNeuronParams
     input_value::Float64 = 0
     gmax::Float64 = 6.0e-9
     tau_ampa::Float64 = 5.0e-3
-    vtarget::Float64 = 0
+    tau_GABAa_rise::Float64 = 1e-3
+    tau_GABAa_fast::Float64 = 6e-3
+    vtarget_exc::Float64 = 0
+    vtarget_inh::Float64 = -75e-3
     vspike::Float64 = 0
     inc_gsyn::Float64 = 1e-9
 end
@@ -108,52 +113,60 @@ struct SIMPLE_E <: SynapseType end
 struct AMPA <: SynapseType end
 struct GABAa <: SynapseType end
 
-function get_synapse(_synapse_type::SIMPLE_E, params)::Equation # _ before variable is a convention for a unused variable in function body ; just used for type dispatch
-    @variables g_syn = 0
-    [g_syn ~ params.gl * params.a]
+function get_synapse_eq(_synapse_type::AMPA, post_neuron::ODESystem)::Equation # _ before variable is a convention for a unused variable in function body ; just used for type dispatch
+    Equation(post_neuron.ampa_syn.g_syn, post_neuron.ampa_syn.g_syn + post_neuron.ampa_syn.inc_gsyn)
+end
+
+function get_synapse_eq(_synapse_type::GABAa, post_neuron::ODESystem)::Equation # _ before variable is a convention for a unused variable in function body ; just used for type dispatch
+    Equation(post_neuron.gabaa_syn.g_syn, post_neuron.gabaa_syn.g_syn + post_neuron.gabaa_syn.inc_gsyn)
 end
 
 function make_neuron(params, soma_model, tspan, name)::ODESystem
     @named soma = soma_model(; name=Symbol("soma"))
     @named ampa_syn = SynapseAMPA(; name=Symbol("ampa_syn"))
-    # @mtkbuild gabaa_syn = SynapseGABAa()
+    @named gabaa_syn = SynapseGABAa(; name=Symbol("gabaa_syn"))
 
     neuron = compose(
         ODESystem(
             [
-                soma.Ie ~ ampa_syn.g_syn * (soma.v - ampa_syn.vtarget)
+                soma.Ie ~ ampa_syn.g_syn * (soma.v - ampa_syn.vtarget_exc)
+                soma.Ii ~ gabaa_syn.g_syn * (soma.v - gabaa_syn.vtarget_inh)
             ], t; name=name
-        ), soma, ampa_syn
+        ), soma, ampa_syn, gabaa_syn
     )
     neuron
 end
-make_events(premises::Vector{Equation}, pre::ODESystem)::Pair{Vector{Equation}, Vector{Equation}} = [pre.soma.v ~ pre.soma.vspike] => [[
-    pre.soma.v ~ pre.soma.vrest,
-    pre.soma.R ~ pre.soma.R + 1,
-    pre.soma.w ~ pre.soma.w + pre.soma.b
-    ] ; premises
-]
+make_events(premises::Vector{Equation}, pre::ODESystem)::Pair{Vector{Equation},Vector{Equation}} = [pre.soma.v ~ pre.soma.vspike] => vcat(
+    [
+        pre.soma.v ~ pre.soma.vrest,
+        pre.soma.R ~ pre.soma.R + 1,
+        pre.soma.w ~ pre.soma.w + pre.soma.b
+    ], premises
+)
 
-function connect_neurons(pre_neuron::ODESystem, post_neurons::Vector{ODESystem})::Pair{Vector{Equation}, Vector{Equation}}
+function connect_neurons(pre_neuron::ODESystem, post_neurons::Vector{ODESystem}, synapse_type::SynapseType, prob::Float64)::Pair{Vector{Equation},Vector{Equation}}
     callbacks::Vector{Equation} = []
     @inbounds for i in 1:length(post_neurons)
-        push!(callbacks,
-            Equation(post_neurons[i].ampa_syn.g_syn,  post_neurons[i].ampa_syn.g_syn + post_neurons[i].ampa_syn.inc_gsyn)
-        )
+        if rand() < prob && pre_neuron.name != post_neurons[i].name
+            push!(callbacks, get_synapse_eq(synapse_type, post_neurons[i]))
+        end
     end
     make_events(callbacks, pre_neuron)
 end
 
-function make_network(neurons)::ODESystem
-    all_callbacks = []
-    @inbounds for i in 1:length(neurons)
-        post_neurons = [neurons[1:i-1]; neurons[i+1:end]] # forward connections
-        push!(all_callbacks, connect_neurons(neurons[i], post_neurons))
+function connect_many_neurons(pre_neurons::Vector{ODESystem}, post_neurons::Vector{ODESystem}, synapse_type::SynapseType, prob::Float64)::Vector{Pair{Vector{Equation},Vector{Equation}}}
+    all_callbacks::Vector{Pair{Vector{Equation},Vector{Equation}}} = []
+    @inbounds for i in 1:length(pre_neurons)
+        push!(all_callbacks, connect_neurons(pre_neurons[i], post_neurons, synapse_type, prob))
     end
-    @mtkbuild network = compose(ODESystem([], t; continuous_events=all_callbacks, name=:connected_neurons), neurons)
+    all_callbacks
 end
 
-function instantiate_params(sys_index, params)::Union{nothing, Pair{Symbol, Union{Float64, Int64}}}
+function make_network(neurons::Vector{ODESystem}, connections::Vector{Pair{Vector{Equation},Vector{Equation}}})::ODESystem
+    @mtkbuild network = compose(ODESystem([], t; continuous_events=connections, name=:connected_neurons), neurons)
+end
+
+function instantiate_params(sys_index, params)::Union{nothing,Pair{Symbol,Union{Float64,Int64}}}
     param_value = hasproperty(params, Symbol(sys_index[end])) ? getproperty(params, Symbol(sys_index[end])) : nothing
     # isnothing(param_value) ? nothing : String(Symbol(join(sys_index, "__"))) => param_value
     isnothing(param_value) ? nothing : sys_index[end] => param_value
@@ -162,20 +175,20 @@ end
 get_varsym_from_syskey(param::SymbolicUtils.BasicSymbolic)::Symbol = Symbol(split(param |> Symbol |> String, "₊")[end])
 get_varsym_from_syskey(param::SubString)::Symbol = Symbol(split(param, "₊")[end])
 
-function instantiate_param(parameter::SymbolicUtils.BasicSymbolic, params::AdExNeuronParams)::Pair{SymbolicUtils.BasicSymbolic, Union{Float64, Int64}}
+function instantiate_param(parameter::SymbolicUtils.BasicSymbolic, params::AdExNeuronParams)::Pair{SymbolicUtils.BasicSymbolic,Union{Float64,Int64}}
     if hasproperty(params, get_varsym_from_syskey(parameter))
         parameter => getproperty(params, get_varsym_from_syskey(parameter))
     end
 end
 
-function instantiate_uparam(parameter::SymbolicUtils.BasicSymbolic, uparams::AdExNeuronUParams)::Pair{SymbolicUtils.BasicSymbolic, Union{Float64, Int64}}
+function instantiate_uparam(parameter::SymbolicUtils.BasicSymbolic, uparams::AdExNeuronUParams)::Pair{SymbolicUtils.BasicSymbolic,Union{Float64,Int64}}
     replaced_parameter = split(parameter |> Symbol |> String, "(")[1]
     if hasproperty(uparams, get_varsym_from_syskey(replaced_parameter))
         parameter => getproperty(uparams, get_varsym_from_syskey(replaced_parameter))
     end
 end
 
-function map_params(network::ODESystem, params::AdExNeuronParams, uparams::AdExNeuronUParams)::Tuple{Vector{Pair{SymbolicUtils.BasicSymbolic, Union{Int64, Float64}}}, Vector{Pair{SymbolicUtils.BasicSymbolic, Union{Int64, Float64}}}}
+function map_params(network::ODESystem, params::AdExNeuronParams, uparams::AdExNeuronUParams)::Tuple{Vector{Pair{SymbolicUtils.BasicSymbolic,Union{Int64,Float64}}},Vector{Pair{SymbolicUtils.BasicSymbolic,Union{Int64,Float64}}}}
     parameters_to_replace = parameters(network)
     uparameters_to_replace = unknowns(network)
 
