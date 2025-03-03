1fb0c9dbb382a14849ee6517171026e08ef1d34c
Rodolphe Marbot <rododhendron@hey.com>
2025-03-03 19:17:49 +0100

    params

--- scripts/range_exp.jl
+++ scripts/range_exp.jl
@@ -88,18 +88,19 @@ stim_schedule = SNN.Params.generate_schedule(stim_params, tspan; is_pseudo_rando
 
     # a & b
     # param_a_range = 1.0e-10:1.0e-10:6.0e-9
-    param_b_range = 2.0e-12:4e-12:9.0e-11
+    # param_b_range = 2.0e-12:4e-12:9.0e-11
 
     # param_a_range = 1.0e-9:0.5e-9:18.0e-9
     # param_b_range = 50:50:1200
 
     # param_to_change_a = :a
     # param_to_change_b = :b
+    param_a_range = 0.01:0.5:2.0
     param_to_change_a = :sigma
 
-    param_a_range = 0.01:0.05:2.0
     # param_b_range = 0.1:0.1:3.0
     # param_to_change_a = :a
+    param_b_range = 1.0e-12:0.5e-11:10.0e-11
     param_to_change_b = :b
     # param_to_change_b = :sigma
 
